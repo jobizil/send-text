e07eeffe2fd8a7fa278ff69a415c385959ad832f
Job Ugbem <ugbemjob57@gmail.com>
2021-05-24 12:40:19 +0100

    Init

---
+++ public/js/main.js
@@ -0,0 +1,29 @@
+const numberInput = document.getElementById("number"),
+	messageInput = (document.getElementById("msg"), (button = document.getElementById("button"))),
+	response = document.querySelector(".response")
+
+button.addEventListener("click", send, false)
+
+const socket = io()
+socket.on("smsStatus", function (data) {
+	if (data.error) {
+		response.innerHTML = "<h5>" + data.error + "</h5>"
+	} else {
+		response.innerHTML = "<h5>Text message sent to " + data.number + "</h5>"
+	}
+})
+
+function send() {
+	const number = numberInput.value.replace(/\D/g, "")
+	const text = messageInput.value
+
+	fetch("/", {
+		method: "post",
+		headers: {
+			"Content-type": "application/json",
+		},
+		body: JSON.stringify({ number: number, text: text }),
+	})
+		.then((res) => console.log(response))
+		.catch((error) => console.log(error))
+}
