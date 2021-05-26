eebe4101d6d5a7a6ab99c52c5515311148439484
Job Ugbem <ugbemjob57@gmail.com>
2021-05-26 11:08:31 +0100

    fix console

--- public/js/main.js
+++ public/js/main.js
@@ -1,29 +1,31 @@
-const numberInput = document.getElementById("number"),
-	messageInput = (document.getElementById("msg"), (button = document.getElementById("button"))),
-	response = document.querySelector(".response")
+const numberInput = document.getElementById('number'),
+  messageInput =
+    (document.getElementById('msg'),
+    (button = document.getElementById('button'))),
+  response = document.querySelector('.response')
 
-button.addEventListener("click", send, false)
+button.addEventListener('click', send, false)
 
 const socket = io()
-socket.on("smsStatus", function (data) {
-	if (data.error) {
-		response.innerHTML = "<h5>" + data.error + "</h5>"
-	} else {
-		response.innerHTML = "<h5>Text message sent to " + data.number + "</h5>"
-	}
+socket.on('smsStatus', function (data) {
+  if (data.error) {
+    response.innerHTML = '<h5>' + data.error + '</h5>'
+  } else {
+    response.innerHTML = '<h5>Text message sent to ' + data.number + '</h5>'
+  }
 })
 
 function send() {
-	const number = numberInput.value.replace(/\D/g, "")
-	const text = messageInput.value
+  const number = numberInput.value.replace(/\D/g, '')
+  const text = messageInput.value
 
-	fetch("/", {
-		method: "post",
-		headers: {
-			"Content-type": "application/json",
-		},
-		body: JSON.stringify({ number: number, text: text }),
-	})
-		.then((res) => console.log(response))
-		.catch((error) => console.log(error))
+  fetch('/', {
+    method: 'post',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify({ number: number, text: text }),
+  })
+    .then(res => response)
+    .catch(error => console.log(error))
 }
