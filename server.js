dbac9c31d22dbafd1d52a1329999dd61500e3203
Job Ugbem <ugbemjob57@gmail.com>
2021-05-26 10:44:41 +0100

    Fix env

--- server.js
+++ server.js
@@ -1,75 +1,85 @@
-import Express from "express"
-import ejs from "ejs"
-import { Server } from "socket.io"
-import Nexmo from "nexmo"
-import path from "path"
-import { CLIENT_RENEG_LIMIT } from "tls"
+import Express from 'express'
+import ejs from 'ejs'
+import { Server } from 'socket.io'
+import Nexmo from 'nexmo'
+import path from 'path'
+import dotenv from 'dotenv'
+dotenv.config()
 
 // Init nexmo
 const nexmo = new Nexmo(
-	{
-		apiKey: "5b4dc915",
-		apiSecret: "K2MOztyoOqeVwc96",
-	},
-	{ debug: true },
+  {
+    apiKey: process.env.API_KEY,
+    apiSecret: process.env.API_SECRET,
+  },
+  { debug: true }
 )
 
 const app = new Express()
 
 // Setup Template Engine
-app.set("view engine", "html")
-app.engine("html", ejs.renderFile)
+app.set('view engine', 'html')
+app.engine('html', ejs.renderFile)
 
 // Public Folder Setup
-const __dirname = path.resolve(path.dirname(""))
-app.use(Express.static(path.join(__dirname, "/public")))
+const __dirname = path.resolve(path.dirname(''))
+app.use(Express.static(path.join(__dirname, '/public')))
 
 app.use(Express.urlencoded({ extended: true }))
 app.use(Express.json())
 
 // Routes
-app.get("/", (req, res) => {
-	res.render("index")
+app.get('/', (_req, res) => {
+  res.render('index')
 })
 
 // Get Forms Submit
-app.post("/", (req, res) => {
-	const number = req.body.number
-	const text = req.body.text
+app.post('/', (req, _res) => {
+  const number = req.body.number
+  const text = req.body.text
 
-	nexmo.message.sendSms("Jobizil", number, text, { type: "unicode" }, (err, responseData) => {
-		if (err) {
-			console.log(err)
-		} else {
-			// console.dir(responseData)
-			// Get response data
-			const { messages } = responseData
-			const { ["message-id"]: id, ["to"]: to, ["error-text"]: error } = messages[0]
+  nexmo.message.sendSms(
+    process.env.FROM,
+    number,
+    text,
+    { type: 'unicode' },
+    (err, responseData) => {
+      if (err) {
+        console.log(err)
+      } else {
+        // Get response data
+        const { messages } = responseData
+        const {
+          ['message-id']: id,
+          ['to']: to,
+          ['error-text']: error,
+        } = messages[0]
 
-			const data = {
-				id,
-				error,
-				number,
-			}
+        const data = {
+          id,
+          error,
+          number,
+        }
 
-			// Emit to client
-			io.emit("smsStatus", data)
-		}
-	})
+        // Emit to client
+        io.emit('smsStatus', data)
+      }
+    }
+  )
 })
 
 const port = process.env.PORT || 3400
 
 const server = app.listen(port, () => {
-	console.log("Server Running")
+  console.log('Server Running')
 })
 
 // Connect to socket.io
 const io = new Server(server)
 
-io.on("connection", (socket) => {
-	console.log("Connected")
-	io.on("disconnect", () => {
-		console.log("Disconnected")
-	})
+io.on('connection', socket => {
+  console.log('Connected')
+  io.on('disconnect', () => {
+    console.log('Disconnected')
+  })
 })
